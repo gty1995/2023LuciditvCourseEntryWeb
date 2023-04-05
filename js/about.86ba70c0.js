e9ef582460615143990ee0a937ed3fd45c925694
郭小白 <guotongyu1995@luciditv.com>
2023-04-05 15:07:45 +0800

    deploy

---
+++ js/about.86ba70c0.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkentry_web"]=self["webpackChunkentry_web"]||[]).push([[443],{7007:function(n,e,t){t.r(e),t.d(e,{default:function(){return f}});var r=t(6252);const u={class:"about"},a=(0,r._)("h1",null,"This is an about page",-1),s=[a];function c(n,e){return(0,r.wg)(),(0,r.iD)("div",u,s)}var i=t(3744);const o={},b=(0,i.Z)(o,[["render",c]]);var f=b}}]);
+//# sourceMappingURL=about.86ba70c0.js.map
